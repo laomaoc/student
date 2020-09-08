7ba3e45808908a7fae5b403549ed0cae04fae755
lao <lao@qq.com>
2020-09-08 11:12:03 +0800

    在公司开发教室查询功能

--- Homework.java
+++ Homework.java
@@ -2,8 +2,11 @@ public class Homework {
     private int id;
     private String name;
     private String pwd;
+<<<<<<< HEAD
     private int phone;
     private int phone1;
+=======
+>>>>>>> 在公司开发教室查询功能
     private void Homework(){
 
     }
